157560a41044ddc4156b93f78ebf16c41db2b58b
Himanshu <himanshu.saraiya09@gmail.com>
2018-11-09 04:08:24 +0530

    Modified database_drivers, added more functions. In other files, added exception handling

--- code/database_create.py
+++ code/database_create.py
@@ -9,42 +9,63 @@
 
 import sqlite3
 def create_database():
-    conn = sqlite3.connect('./database/database.db')
-    conn.execute('''
-                 CREATE TABLE cluster_db
-                 (s_no INTEGER PRIMARY KEY AUTOINCREMENT,
-                 cluster_id INT NOT NULL,
-                 database TEXT NOT NULL,
-                 FOREIGN KEY(cluster_id) REFERENCES cluster_ip(cluster_id) ON DELETE CASCADE ON UPDATE CASCADE);
-                 ''')
-    print("Table created successfully")
-    conn.execute('''
-                 CREATE TABLE credentials
-                 (username TEXT PRIMARY KEY NOT NULL,
-                 password TEXT NOT NULL);
-                 ''')
-    print("Table created successfully")
-    conn.execute('''
-                 CREATE TABLE cluster_ip
-                 (s_no INTEGER PRIMARY KEY AUTOINCREMENT,
-                 cluster_id INT NOT NULL,
-                 ip TEXT NOT NULL);
-                 ''')
-    print("Table created successfully")
-    conn.execute('''
-                 CREATE TABLE user_db
-                 (s_no INTEGER PRIMARY KEY AUTOINCREMENT,
-                 cluster_id INT NOT NULL,
-                 database TEXT NOT NULL,
-                 FOREIGN KEY(cluster_id) REFERENCES cluster_ip(cluster_id) ON DELETE CASCADE ON UPDATE CASCADE);
-                 ''')
-    print("Table created successfully")
-    conn.execute('''
-                 CREATE TABLE user_details
-                 (username TEXT PRIMARY KEY,
-                 name TEXT NOT NULL,
-                 contact_details INT,
-                 FOREIGN KEY(username) REFERENCES credentials(username) ON DELETE CASCADE ON UPDATE CASCADE);
-                 ''')
-    print("Table created successfully")
+    try:
+        conn = sqlite3.connect('./database/database.db')
+        conn.execute('''
+                     CREATE TABLE cluster_db
+                     (s_no INTEGER PRIMARY KEY AUTOINCREMENT,
+                     cluster_id INT NOT NULL,
+                     database TEXT NOT NULL,
+                     FOREIGN KEY(cluster_id) REFERENCES cluster_ip(cluster_id) ON DELETE CASCADE ON UPDATE CASCADE);
+                     ''')
+        print("Table created successfully")
+        conn.execute('''
+                     CREATE TABLE credentials
+                     (username TEXT PRIMARY KEY NOT NULL,
+                     password TEXT NOT NULL);
+                     ''')
+        print("Table created successfully")
+        conn.execute('''
+                     CREATE TABLE cluster_ip
+                     (s_no INTEGER PRIMARY KEY AUTOINCREMENT,
+                     cluster_id INT NOT NULL,
+                     ip TEXT NOT NULL);
+                     ''')
+        print("Table created successfully")
+        conn.execute('''
+                     CREATE TABLE user_db
+                     (s_no INTEGER PRIMARY KEY AUTOINCREMENT,
+                     cluster_id INT NOT NULL,
+                     database TEXT NOT NULL,
+                     FOREIGN KEY(cluster_id) REFERENCES cluster_ip(cluster_id) ON DELETE CASCADE ON UPDATE CASCADE);
+                     ''')
+        print("Table created successfully")
+        conn.execute('''
+                     CREATE TABLE user_details
+                     (username TEXT PRIMARY KEY,
+                     name TEXT NOT NULL,
+                     contact_details INT,
+                     FOREIGN KEY(username) REFERENCES credentials(username) ON DELETE CASCADE ON UPDATE CASCADE);
+                     ''')
+        print("Table created successfully")
+        conn.execute('''
+                     CREATE TABLE db_lock
+                     (database TEXT PRIMARY KEY,
+                     lock integer NOT NULL,
+                     FOREIGN KEY(database) REFERENCES cluster_db(database) ON DELETE CASCADE ON UPDATE CASCADE);
+                     ''')
+        print("Table created successfully")
+        conn.execute('''
+                     CREATE TABLE ip_node_status
+                     (ip TEXT PRIMARY KEY,
+                     status integer NOT NULL,
+                     database TEXT NOT NULL,
+                     FOREIGN KEY(database) REFERENCES cluster_db(database) ON DELETE CASCADE ON UPDATE CASCADE,
+                     FOREIGN KEY(ip) REFERENCES cluster_ip(ip) ON DELETE CASCADE ON UPDATE CASCADE);
+                     ''')
+        print("Table created successfully")
+    except Exception as e:
+        print("Exception [database_create.py:Database] ::-",str(e))
     conn.close()
+create_database()
+    
--- code/database_insertion_drivers.py
+++ code/database_insertion_drivers.py
@@ -10,58 +10,66 @@
 
 import sqlite3
 
+def executeQuery(query_string):
+    """
+        This function executes the query passed as 'query_string'. User should make sure query is error free. Use with caution.
+    """
+    try:
+        conn = sqlite3.connect('./database/database.db')
+        conn.execute(query_string)
+    except Exception as e:
+        print("Exception [database_insertion_drivers.py:Database] ::-",str(e))
+    conn.commit()
+    conn.close()
+
 def add_user_details(username,name,contact_details):
-    conn = sqlite3.connect('./database/database.db')
-    conn.execute(
-                'insert into user_details (username,name,contact_details) values(\''+str(username)+'\',\''+str(name)+'\','+str(contact_details)+')'
-                )
-    conn.commit()
-    conn.close()
+    executeQuery('insert into user_details (username,name,contact_details) values(\''+str(username)+'\',\''+str(name)+'\','+str(contact_details)+');')
 
+def add_credential(username,password):
+    executeQuery('insert into credentials (username,password) values(\''+str(username)+'\',\''+str(password)+'\');')
+    
+def add_db_in_cluster(cluster_id,db_name):
+    executeQuery('insert into cluster_db(cluster_id,database) values(\''+str(cluster_id)+',\''+str(db_name)+'\');')
+    
+def add_node_in_cluster(cluster_id,ip):
+    executeQuery('insert into cluster_ip(cluster_id,ip) values(\''+str(cluster_id)+',\''+str(ip)+'\');')
+    
+def add_user_db(cluster_id,db_name):
+    executeQuery('insert into user_db(cluster_id,database) values(\''+str(cluster_id)+',\''+str(db_name)+'\');')
+    
+def add_lock_status(ip,status,db_name):
+    query = 'insert into ip_node_status(ip,status,database) values(\''+(ip)+'\','+str(status)+',\''+str(db_name)+'\');'
+    executeQuery(query)
+    
+def add_db_lock(db_name,lock):
+    query = 'insert into db_lock(database,lock) values(\''+(db_name)+'\','+str(lock)+');'
+    executeQuery(query)
+    
+def getLockFromDB(db_name):
+    query = 'select lock from db_lock where database =\'' + db_name + '\';'
+    try:
+        conn = sqlite3.connect('./database/database.db')
+        cursor = conn.execute(query)
+        value = cursor.fetchall()
+        lock_val = value[0][0]
+    except Exception as e:
+        print("Exception [database_insertion_drivers.py:Database] ::-",str(e))
+    conn.commit()
+    conn.close()
+    return lock_val
 
-def add_credential(credential):
-    username1,password1 = credential
-    conn = sqlite3.connect('./database/database.db')
-    conn.execute(
-                'insert into credentials (username,password) values(\''+str(username1)+'\',\''+str(password1)+'\')'
-                )
-    print('Value inserted',username1,' ',password1)
+def getStatusFromIP(ip,db_name):
+    query = 'select status from ip_node_status where ip =\'' + ip + '\' and database = \'' + db_name + '\';'
+    try:
+        conn = sqlite3.connect('./database/database.db')
+        cursor = conn.execute(query)
+        value = cursor.fetchall()
+        status = value[0][0]
+    except Exception as e:
+        print("Exception [database_insertion_drivers.py:Database] ::-",str(e))
     conn.commit()
     conn.close()
+    return status
     
-def add_db_in_cluster(cluster_db_tuple):
-    conn = sqlite3.connect('./database/database.db')
-    for cluster_db_pair in cluster_db_tuple:
-        cluster_id, nodes = cluster_db_pair
-        for db in nodes:
-            conn.execute(
-                        'insert into cluster_db(cluster_id,database) values('+str(cluster_id)+',\''+str(db)+'\')'
-                        )
-            print('Value inserted',cluster_id,' ',db)
-    conn.commit()
-    conn.close()
-    
-def add_node_in_cluster(cluster_ip_tuple):
-    conn = sqlite3.connect('./database/database.db')
-    for cluster_ip_pair in cluster_ip_tuple:
-        cluster_id, nodes = cluster_ip_pair
-        for ip in nodes:
-            conn.execute(
-                        'insert into cluster_ip(cluster_id,ip) values('+str(cluster_id)+',\''+str(ip)+'\')'
-                        )
-            print('Value inserted',cluster_id,' ',ip)
-    conn.commit()
-    conn.close()
-    
-def add_user_db(user_db_tuple):
-    conn = sqlite3.connect('./database/database.db')
-    for user_db_pair in user_db_tuple:
-        cluster_id, dbs = user_db_pair
-        for db in dbs:
-            conn.execute(
-                        'insert into user_db(cluster_id,database) values('+str(cluster_id)+',\''+str(db)+'\')'
-                        )
-            print('Value inserted',cluster_id,' ',db)
-    conn.commit()
-    conn.close()
+getStatusFromIP('1.1.1.1','abdb')
     
