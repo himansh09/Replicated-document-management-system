35a3cdf2a394bd085ae2f95a74aad344f56f1142
Himanshu <himanshu.saraiya09@gmail.com>
2018-11-10 16:57:25 +0530

    added global variable sharing

--- code/replicated_document_management_system/client_setup.py
+++ code/replicated_document_management_system/client_setup.py
@@ -7,11 +7,10 @@
 *** Roll Number: 18111023
 """
 import configparser
-
+import global_variables
 
 conf = configparser.ConfigParser()
 conf.read('conf/client.conf')
-server_ip = conf['client_conf']['ip']
-server_prefix = conf['client_conf']['prefix']
-print(server_ip)
-print(server_prefix)
+client_ip = conf['client_conf']['ip']
+client_prefix = conf['client_conf']['prefix']
+global_variables.my_ip = client_ip
---
+++ code/replicated_document_management_system/global_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+*** Created using spyder 3.2.8
+*** Created on Sat Nov 10 16:26:44 2018
+*** Author : Himanshu Saraiya
+*** Roll Number: 18111023
+"""
+
+global my_ip
+global my_prefix
+
+my_ip = '127.0.0.1'
+my_prefix = '/'
